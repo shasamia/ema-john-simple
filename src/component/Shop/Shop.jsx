f47ec6601d2b215f89dfd67dba6bfd19136b5a88
shasamia <shasamia006@gmail.com>
2023-03-27 22:47:00 +0600

    added to the cart

--- src/component/Shop/Shop.jsx
+++ src/component/Shop/Shop.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
+
 import Product from '../Product/Product';
 import './Shop.css'
 
@@ -24,8 +26,7 @@ const Shop = () => {
                }
             </div>
             <div className="cart-container">
-                <h4>Order Summary</h4>
-                <p>Selectd Items: {cart.length}</p>
+              <Cart cart={cart}></Cart>
             </div>
             
         </div>
